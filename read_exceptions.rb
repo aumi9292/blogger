305d14b09df9346347a309cc6d1911801ab9ed5c
Austin Miller <austin.miller@colorado.edu>
2020-12-27 19:13:54 -0700

    read and display counts of exception types

--- read_exceptions.rb
+++ read_exceptions.rb
@@ -1,11 +1,38 @@
 require 'yaml'
 
-exceptions = []
-
-File.open('structured_exceptions.yml') do |yaml_file|
-  YAML.load_stream(yaml_file) do |yaml_doc|
-    exceptions << yaml_doc 
-  end
+def read_all_exceptions
+  exceptions = []
+  File.open('structured_exceptions.yml') do |yaml_file|
+    YAML.load_stream(yaml_file) do |yaml_doc|
+      exceptions << yaml_doc 
+    end
+  end 
+  exceptions
 end 
 
-puts exceptions
+def get_total_project_exceptions(exceptions)
+  exceptions.length
+end
+
+def get_count_of_each_exception_class(exceptions)
+  counts = {}
+  exceptions.each do |exception|
+    type = exception[:type]
+    counts[type] ? counts[type] += 1 : counts[type] = 1
+  end 
+  counts
+end 
+
+def display_counts(counts)
+  system('clear')
+  counts.each do |type, count|
+    puts  "\n"
+    puts "Exception Class: #{type}"
+    puts "Total: #{count}"
+    puts "-" * 80
+  end
+end
+
+exceptions = read_all_exceptions
+counts = get_count_of_each_exception_class(exceptions)
+display_counts(counts)
