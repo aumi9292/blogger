b5cd22a5d6b96c008664a974478ad991ff0f48a7
Austin Miller <austin.miller@colorado.edu>
2020-12-28 18:47:22 -0700

    added logic for displaying error class and line no for a specified file

--- read_exceptions.rb
+++ read_exceptions.rb
@@ -10,6 +10,29 @@ def read_all_exceptions
   exceptions
 end 
 
+def read_exceptions_for_file(filename)
+  exceptions = read_all_exceptions
+  exceptions.select do |exc|
+    exc[:filename] == filename
+  end 
+end 
+
+def display_exceptions_for_file(filename)
+  puts "\n"
+  puts "File: #{filename}"
+  puts '-' * 80
+  exceptions = read_exceptions_for_file(filename)
+  exceptions.each do |exc|
+    format_type_and_line(exc[:type], exc[:line_number])
+  end 
+  puts '-' * 80
+end 
+
+def format_type_and_line(type, line)
+  type_length = type.length
+  puts "Class: #{type}" + " " * (50 - type_length) + "Line: #{line}"
+end 
+
 def get_total_project_exceptions(exceptions)
   exceptions.length
 end
@@ -25,9 +48,7 @@ end
 
 def display_total_and_counts(total, counts)
   system('clear')
-  
   puts "Total Exceptions: #{total}"
-
   counts.each do |type, count|
     puts  "\n"
     puts "Exception Class: #{type}"
@@ -36,7 +57,17 @@ def display_total_and_counts(total, counts)
   end
 end
 
-exceptions = read_all_exceptions
-total = get_total_project_exceptions(exceptions)
-counts = get_count_of_each_exception_class(exceptions)
-display_total_and_counts(total, counts)
+def display_summary
+  exceptions = read_all_exceptions
+  total = get_total_project_exceptions(exceptions)
+  counts = get_count_of_each_exception_class(exceptions)
+  display_total_and_counts(total, counts)
+end 
+
+specified_file = ARGV[0]
+
+if specified_file && File.exist?(specified_file)
+  display_exceptions_for_file(specified_file) 
+else 
+  display_summary
+end 
