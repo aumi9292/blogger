5ace4452bcadbfcff4cec5123b609e1927660ad2
Austin Miller <austin.miller@colorado.edu>
2021-02-13 19:40:25 -0700

    100 percent test coverage for BloggerException.rb and quick correction for BloggerException#to_s method

--- test/blog_exception_test.rb
+++ test/blog_exception_test.rb
@@ -1,3 +1,5 @@
+# require 'simplecov'
+# SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'fileutils'
@@ -15,6 +17,8 @@ class BlogTest < MiniTest::Test
     @out, @err = capture_subprocess_io do
       run_project_file
     end
+
+    @blogger_exception = YAML.load(File.read('structured_exceptions.yml'))
 
   end 
 
@@ -40,13 +44,45 @@ class BlogTest < MiniTest::Test
 
   def test_log_confirmation_displays
     assert_match(/Bug Logged Successfully:/, @out)
-
     assert_match(/ZeroDivisionError/, @err)
   end 
 
   def test_blogger_instance
     blogger_yaml = YAML.load(File.read('structured_exceptions.yml'))
     assert_instance_of(BloggerException, blogger_yaml)
+  end
+
+  def test_blogger_to_s
+    assert_instance_of(String, @blogger_exception.to_s)
+  end
+
+  def test_blogger_to_s_keys
+    assert_match(/:file:/, @blogger_exception.to_s)
+  end
+
+  def test_blogger_file_to_s
+    assert_match(/main.rb/, @blogger_exception.file)
+  end 
+
+  def test_display
+    constant_display_msg = "\tClass ZeroDivisionError\n\tDescription: Attempting to divide an Integer by 0."
+    assert_output(/#{constant_display_msg}/) { @blogger_exception.display }
+  end
+
+  def test_generate_description_for_zero_div_exception
+    type = @blogger_exception.type.to_sym
+    assert_equal( "Attempting to divide an Integer by 0.", @blogger_exception.generate_description(type))
+  end
+
+  def test_generate_description_for_other_exception
+    default_msg = 'Check out https://ruby-doc.org/core-2.7.0/Exception.html for more information.'
+    assert_equal(default_msg, @blogger_exception.generate_description('SomeExceptionClass'))
+  end
+
+  def test_private_getters
+    assert_raises(NoMethodError) { @blogger_exception.cause }
+    assert_raises(NoMethodError) { @blogger_exception.message }
+    assert_raises(NoMethodError) { @blogger_exception.scope }
   end
 
   def teardown
